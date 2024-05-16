63db35672010115c2e99d576ae07dda8dabb2174
yashk9293 <yashk9293@gmail.com>
2024-05-17 01:49:49 +0530

    initial

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -10,9 +10,8 @@ import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
 import { myCache } from "../app.js";
 import { invalidateCache } from "../utils/features.js";
-import {v2 as cloudinary} from "cloudinary"
 
-// Revalidate on New,Update,Delete Product & on New Order
+
 export const getlatestProducts = TryCatch(async (req, res, next) => {
   let products;
 
@@ -29,7 +28,7 @@ export const getlatestProducts = TryCatch(async (req, res, next) => {
   });
 });
 
-// Revalidate on New,Update,Delete Product & on New Order
+
 export const getAllCategories = TryCatch(async (req, res, next) => {
   let categories;
 
@@ -46,7 +45,7 @@ export const getAllCategories = TryCatch(async (req, res, next) => {
   });
 });
 
-// Revalidate on New,Update,Delete Product & on New Order
+
 export const getAdminProducts = TryCatch(async (req, res, next) => {
   let products;
   if (myCache.has("all-products"))
@@ -82,15 +81,11 @@ export const getSingleProduct = TryCatch(async (req, res, next) => {
 });
 
 export const newProduct = TryCatch(
-  async (req, res, next) => {
+  async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
     const { name, price, stock, category } = req.body;
     const photo = req.file;
 
     if (!photo) return next(new ErrorHandler("Please add Photo", 400));
-
-    const myCloud = await cloudinary.uploader.upload(photo.path, {
-      folder: "products",
-    });
 
     if (!name || !price || !stock || !category) {
       rm(photo.path, () => {
@@ -105,7 +100,7 @@ export const newProduct = TryCatch(
       price,
       stock,
       category: category.toLowerCase(),
-      photo: myCloud.secure_url,
+      photo: photo.path,
     });
 
     invalidateCache({ product: true, admin: true });
@@ -117,6 +112,7 @@ export const newProduct = TryCatch(
   }
 );
 
+
 export const updateProduct = TryCatch(async (req, res, next) => {
   const { id } = req.params;
   const { name, price, stock, category } = req.body;
@@ -125,15 +121,11 @@ export const updateProduct = TryCatch(async (req, res, next) => {
 
   if (!product) return next(new ErrorHandler("Product Not Found", 404));
 
-  const myCloud = await cloudinary.uploader.upload(photo!.path, {
-    folder: "products",
-  });
-
   if (photo) {
     rm(product.photo!, () => {
       console.log("Old Photo Deleted");
     });
-    product.photo = myCloud.secure_url;
+    product.photo = photo.path;
   }
 
   if (name) product.name = name;
@@ -222,37 +214,3 @@ export const getAllProducts = TryCatch(
     });
   }
 );
-
-// const generateRandomProducts = async (count: number = 10) => {
-//   const products = [];
-
-//   for (let i = 0; i < count; i++) {
-//     const product = {
-//       name: faker.commerce.productName(),
-//       photo: "uploads\\5ba9bd91-b89c-40c2-bb8a-66703408f986.png",
-//       price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
-//       stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
-//       category: faker.commerce.department(),
-//       createdAt: new Date(faker.date.past()),
-//       updatedAt: new Date(faker.date.recent()),
-//       __v: 0,
-//     };
-
-//     products.push(product);
-//   }
-
-//   await Product.create(products);
-
-//   console.log({ succecss: true });
-// };
-
-// const deleteRandomsProducts = async (count: number = 10) => {
-//   const products = await Product.find({}).skip(2);
-
-//   for (let i = 0; i < products.length; i++) {
-//     const product = products[i];
-//     await product.deleteOne();
-//   }
-
-//   console.log({ succecss: true });
-// };
